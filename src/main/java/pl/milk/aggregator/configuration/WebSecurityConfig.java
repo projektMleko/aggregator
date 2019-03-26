d9e4872be7d13928d8361ea276d0dabb6c2a83b4
projektMleko <48417915+projektMleko@users.noreply.github.com>
2019-03-26 20:22:05 +0100

    feature/login_page (#4)
    * simple login page
    
    * force loginpage when get metrics from actuator

--- src/main/java/pl/milk/aggregator/configuration/WebSecurityConfig.java
+++ src/main/java/pl/milk/aggregator/configuration/WebSecurityConfig.java
@@ -13,8 +13,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/h2/**").authenticated()
+                .antMatchers("/actuator/**").authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login");
 
         http.csrf().disable();
         http.headers().frameOptions().disable();
--- src/main/java/pl/milk/aggregator/controller/WebController.java
+++ src/main/java/pl/milk/aggregator/controller/WebController.java
@@ -10,4 +10,9 @@ public class WebController {
     public String homePage() {
         return "home";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
