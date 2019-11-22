1c5ec5ae913c398b1fe4faeb4109146a48b62e2b
sensibles <konik123>
2019-11-22 13:56:07 +0100

    kafka module with docker-compose file

--- src/main/java/pl/milk/aggregator/persistance/repository/RatingRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.milk.aggregator.persistance.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.milk.aggregator.persistance.model.Rating;
-
-@Repository
-public interface RatingRepository extends JpaRepository<Rating, Long> {
-}
--- src/main/java/pl/milk/aggregator/service/BatchInsertSerivce.java
+++
@@ -1,8 +0,0 @@
-package pl.milk.aggregator.service;
-
-import java.util.List;
-
-
-public interface BatchInsertSerivce<T> {
-    List<T> batchInsert(final List<T> group);
-}
